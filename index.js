72db437d3ad033b3ac208ccf88f53167dc0eaf5a
github username <scott.gloyna01@gmail.com>
2021-02-19 22:32:52 -0600

    commit tree de666d218006033e77c19b8ee26f08fb5f6ddc82

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solutions in this file
+
+// for ([initialization]; [condition]; [iteration]) {
+//     [loop body]
+//   } 
+
+// const gifts = ["teddy bear", "drone", "doll"];
+
+// function wrapGifts(gifts) {
+//   for (let i = 0; i < gifts.length; i++) {
+//     console.log(`Wrapped ${gifts[i]} and added a bow!`);
+//   }
+
+//   return gifts;
+// }
+
+function writeCards( namesArray, event ) {
+    let cards = []
+    for ( let i = 0; i < namesArray.length; i++ ) {
+        cards.push( `Thank you, ${namesArray[i]}, for the wonderful ${event} gift!` )
+    }
+    return cards
+}
+
+function countDown(number) {
+    while (number >= 0) {
+        console.log(number);
+        number--;
+    }
+}
